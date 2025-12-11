52dbf32f3abc27acf17080ad49ecbf50ec357616
Yousef Alkurdi <yalkerdi@gmail.com>
2025-12-11 17:18:56 +0100

    Add files via upload

---
+++ J2F1BELP5L2 - Content uit je database/includes/footer.php
@@ -0,0 +1,5 @@
+<!-- jouw HTML voor een Footer komt hier... 
+Benoem hier ten minste je naam en de tijd
+-->
+
+<footer> &copy YAlkurdi <?= date("Y")?></footer> 
---
+++ J2F1BELP5L2 - Content uit je database/includes/header.php
@@ -0,0 +1,16 @@
+<!-- jouw HTML voor een Header komt hier... 
+Gebruik hier tenminste een header afbeelding en een menu
+Zorg dat je in het menu bij elk item een url parameter zet
+om te bepalen welke inhoud er ingeladen moet worden in je html
+-->
+
+<header>
+    <div class="menu">
+        <nav>
+            <a href="">php</a>
+            <a href="">sql</a>
+            <a href="">java</a>
+        </nav>
+    </div>
+</header>
+
